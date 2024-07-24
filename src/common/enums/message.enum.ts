cd517d136e08400641900e475891dc1cf5170e04
MostafaMemari <m.memari7@yahoo.com>
2024-07-24 18:41:45 +0330

    ✨ feat: update and find one and delete category

--- src/common/enums/message.enum.ts
+++ src/common/enums/message.enum.ts
@@ -14,10 +14,18 @@ export enum PublicMessage {
   SendOtp = 'کد یکبار مصرف با موفقیت ارسال شد',
   LoggedIn = 'با موفقیت وارد حساب کاربری خود شدید',
   Created = 'با موفقیت ایجاد شد',
+  Deleted = 'با موفقیت حذف شد',
+  Updated = 'با موفقیت به روزرسانی شد',
+  Inserted = 'با موفقیت درج شد',
 }
 export enum ConflictMessage {
   CategoryTitle = 'عنوان دسته بندی قبلا ثبت شده است',
 }
 
-export enum NotFoundMessage {}
+export enum NotFoundMessage {
+  NotFound = 'موردی یافت نشد',
+  NotFoundCategory = 'دسته بندی یافت نشد',
+  NotFoundPost = 'دسته بندی یافت نشد',
+  NotFoundUser = 'کاربری یافت نشد',
+}
 export enum ValidationMessage {}
