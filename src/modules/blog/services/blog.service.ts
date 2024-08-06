f83736c2c2aecfb02e335df911c98afdef5a61f6
MostafaMemari <m.memari7@yahoo.com>
2024-08-06 17:35:56 +0330

    ✨ feat: randomly fetching suggested articles

--- src/modules/blog/services/blog.service.ts
+++ src/modules/blog/services/blog.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, forwardRef, Inject, Injectable, NotFoundException, Scope } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BlogEntity } from '../entities/blog.entity';
-import { FindOptionsWhere, Repository } from 'typeorm';
+import { DataSource, FindOptionsWhere, Repository } from 'typeorm';
 import { CreateBlogDto, FilterBlogDto, UpdateBlogDto } from '../dto/blog.dto';
 import { createSlug, randomId } from 'src/common/utils/functions.util';
 import { BlogStatus } from '../enum/status.enum';
@@ -29,6 +29,7 @@ export class BlogService {
     @InjectRepository(BlogCategoryEntity) private blogCategoryRepository: Repository<BlogCategoryEntity>,
     @Inject(REQUEST) private request: Request,
     private categoryService: CategoryService,
+    private dataSource: DataSource,
 
     private blogCommentService: BlogCommentService,
   ) {}
@@ -272,37 +273,70 @@ export class BlogService {
     const userId = this.request?.user?.id;
     const blog = await this.blogRepository
       .createQueryBuilder(EntityName.Blog)
-      .where({ slug })
       .leftJoin('blog.categories', 'categories')
       .leftJoin('categories.category', 'category')
       .leftJoin('blog.author', 'author')
       .leftJoin('author.profile', 'profile')
       .addSelect(['categories.id', 'category.title', 'author.username', 'author.id', 'profile.nick_name'])
+      .where({ slug })
       .loadRelationCountAndMap('blog.likes', 'blog.likes')
       .loadRelationCountAndMap('blog.bookmarks', 'blog.bookmarks')
-      // .leftJoinAndSelect('blog.comments', 'comments', 'comments.accepted = :accepted', { accepted: true })
-      .orderBy('blog.id', 'DESC')
       .getOne();
-
-    if (!blog) throw new NotFoundException(NotFoundMessage.NotFound);
-
-    const commentData = await this.blogCommentService.findCommentsOfBlog(blog.id, paginationDto);
-
+    if (!blog) throw new NotFoundException(NotFoundMessage.NotFoundPost);
+    const commentsData = await this.blogCommentService.findCommentsOfBlog(blog.id, paginationDto);
     let isLiked = false;
     let isBookmarked = false;
-
-    if (userId && isNaN(userId) && userId > 0) {
+    if (userId && !isNaN(userId) && userId > 0) {
       isLiked = !!(await this.blogLikeRepository.findOneBy({ userId, blogId: blog.id }));
       isBookmarked = !!(await this.blogBookmarkRepository.findOneBy({ userId, blogId: blog.id }));
     }
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    const suggestBlogs = await queryRunner.query(`
+        WITH suggested_blogs AS (
+            SELECT 
+                blog.id,
+                blog.slug,
+                blog.title,
+                blog.description,
+                blog.time_for_study,
+                blog.image,
+                json_build_object(
+                    'username', u.username,
+                    'author_name', p.nick_name,
+                    'image', p.image_profile
+                ) AS author,
+                array_agg(DISTINCT cat.title) AS categories,
+                (
+                    SELECT COUNT(*) FROM blog_likes
+                    WHERE blog_likes."blogId" = blog.id
+                ) AS likes,
+                (
+                    SELECT COUNT(*) FROM blog_bookmarks
+                    WHERE blog_bookmarks."blogId" = blog.id
+                ) AS bookmarks,
+                (
+                    SELECT COUNT(*) FROM blog_comments
+                    WHERE blog_comments."blogId" = blog.id
+                ) AS comments
+            FROM blog
+            LEFT JOIN public.user u ON blog."authorId" = u.id
+            LEFT JOIN profile p ON p."userId" = u.id
+            LEFT JOIN blog_category bc ON blog.id = bc."blogId"
+            LEFT JOIN category cat ON bc."categoryId" = cat.id
+            GROUP BY blog.id, u.username, p.nick_name, p.image_profile
+            ORDER BY RANDOM()
+            LIMIT 3
 
-    // const blogData = { isLiked, isBookmarked, ...blog };
-
+        )
+        SELECT * FROM suggested_blogs
+    `);
     return {
       blog,
       isLiked,
       isBookmarked,
-      commentData,
+      commentsData,
+      suggestBlogs,
     };
   }
 }
