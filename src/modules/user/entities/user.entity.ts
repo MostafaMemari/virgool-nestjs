36a27726b50bfbf492b54faf1b7ecfc0e54b967a
MostafaMemari <m.memari7@yahoo.com>
2024-08-09 11:18:47 +0330

    ✨ feat: migration database

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -15,65 +15,50 @@ import { FollowEntity } from './follow.entity';
 export class UserEntity extends BaseEntity {
   @Column({ unique: true, nullable: true })
   username: string;
-
   @Column({ nullable: true })
   password: string;
-
   @Column({ unique: true, nullable: true })
   email: string;
-
+  @Column({ nullable: true, type: 'numeric', default: 0 })
+  balance: number;
   @Column({ default: Roles.User })
   role: string;
-
   @Column({ type: 'varchar', nullable: true })
   new_email: string | null;
   @Column({ nullable: true })
   verify_email: boolean;
-
   @Column({ unique: true, nullable: true })
   phone: string;
   @Column({ type: 'varchar', nullable: true })
   new_phone: string | null;
   @Column({ nullable: true })
   verify_phone: boolean;
-
   @Column({ nullable: true })
   otpId: number;
   @OneToOne(() => OtpEntity, (otp) => otp.user, { nullable: true })
   @JoinColumn()
   otp: OtpEntity;
-
   @Column({ type: 'varchar', nullable: true })
   status: string | null;
-
   @Column({ nullable: true })
   profileId: number;
-
   @OneToOne(() => ProfileEntity, (profile) => profile.user, { nullable: true })
   @JoinColumn()
   profile: ProfileEntity;
-
   @OneToMany(() => BlogEntity, (blog) => blog.author)
   blogs: BlogEntity[];
-
   @OneToMany(() => BlogLikesEntity, (like) => like.user)
   blog_likes: BlogLikesEntity[];
-
   @OneToMany(() => BlogBookmarkEntity, (bookmark) => bookmark.user)
   blog_bookmarks: BlogBookmarkEntity[];
-
   @OneToMany(() => ImageEntity, (image) => image.user)
   images: ImageEntity[];
-
   @OneToMany(() => BlogCommentEntity, (comment) => comment.user)
   blog_comments: BlogCommentEntity[];
-
   @OneToMany(() => FollowEntity, (follow) => follow.following)
   followers: FollowEntity[];
-
   @OneToMany(() => FollowEntity, (follow) => follow.follower)
   following: FollowEntity[];
-
   @CreateDateColumn()
   created_at: Date;
   @UpdateDateColumn()
